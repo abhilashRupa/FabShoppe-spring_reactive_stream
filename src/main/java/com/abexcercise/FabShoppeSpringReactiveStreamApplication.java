b4168cfc9e3bbb6f9ea6365b97a818187011ea06
Abhilash Rupa <abhilashrupa@gmail.com>
2025-11-11 17:19:44 +0530

    initial setup commit

---
+++ src/main/java/com/abexcercise/FabShoppeSpringReactiveStreamApplication.java
@@ -0,0 +1,13 @@
+package com.abexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabShoppeSpringReactiveStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabShoppeSpringReactiveStreamApplication.class, args);
+	}
+
+}
